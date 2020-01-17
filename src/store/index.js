286207fe4ad7e74b69ae10b194c88712db7fc49a
Alexander <nth.alexx@gmail.com>
2020-01-17 23:12:15 +0200

    updates

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,8 @@
-import {createStore, combineReducers, applyMiddleware, compose} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
-import auth from './auth'
+import rootReducer from './reducers'
 
 const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
-const rootReducer = combineReducers({
-    auth
-})
 
 const store = createStore(rootReducer, enhancer(
     applyMiddleware(thunk)
